b0566f1d130a89c202b12d9c9ab84ce741205b3a
dinesh-veer <dveer123@hotmail.com>
2026-02-20 15:07:54 +0530

    adding pattern matching example

--- src/main/java/com/dinesh/patternMatchingWithPrimitive/InstanceofPrimitivePattern.java
+++ src/main/java/com/dinesh/patternMatchingWithPrimitive/InstanceofPrimitivePattern.java
@@ -1,4 +1,31 @@
 package com.dinesh.patternMatchingWithPrimitive;
 
+/**
+ * This example shows conversion without losing data
+ * ✔ Matches only if conversion is lossless
+ * ❌ No cast, no manual range check
+ */
 public class InstanceofPrimitivePattern {
+
+    static void checkByteType(int value){
+        if (value instanceof byte b) {
+            System.out.println("Fits in byte: " + b);
+        } else {
+            System.out.println(String.format("%d Does not fit in byte",value));
+        }
+    }
+    static void main() {
+
+        System.out.println("---------------------------------------------");
+
+        System.out.println("Will Integer 100 fits into byte : " );
+        checkByteType(100);
+
+        System.out.println("Will Integer 1000 fits into byte : " );
+        checkByteType(1000);
+
+
+        System.out.println("---------------------------------------------");
+
+    }
 }
