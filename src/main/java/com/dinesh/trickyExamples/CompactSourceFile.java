afe282b9bcdf2ac5ac2369e19997b3120cc41fa1
dinesh-veer <dveer123@hotmail.com>
2026-03-26 10:45:19 +0530

    Compact source file example

---
+++ src/main/java/com/dinesh/trickyExamples/CompactSourceFile.java
@@ -0,0 +1,7 @@
+//Below will give compile time error as
+//package statement is not allowed in compact source files
+//package com.dinesh.trickyExamples;
+//This will compile and print (Compact Source File) No class required
+void main() {
+    System.out.println("Hello Java 25");
+}
