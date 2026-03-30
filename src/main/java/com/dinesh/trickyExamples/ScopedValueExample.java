fece0ced847fb08952c08b5a32736f4b6a53f07d
dinesh-veer <dveer123@hotmail.com>
2026-03-30 09:02:50 +0530

    Scoped value Example to get values from scoped variable

---
+++ src/main/java/com/dinesh/trickyExamples/ScopedValueExample.java
@@ -0,0 +1,38 @@
+package com.dinesh.trickyExamples;
+
+public class ScopedValueExample {
+
+
+    static final ScopedValue<String> USER = ScopedValue.newInstance();
+
+    static void main(String[] args) {
+
+        System.out.println("----------------------------------------------------");
+
+        System.out.println(USER.get());//Exception will thrown NoSuchElementException
+
+        //ScopedValue does not have a "default" value, and it is completely empty when you create it with newInstance().
+        // Before you can call .get(), you must bind a value to it for your current execution scope.
+        //
+        //Because your code calls USER.get() without ever binding a value to USER first, Java panics and throws a NoSuchElementException
+        // because there is nothing to get.
+
+        //To fix this you can use below code
+
+        System.out.println("----------------------------------------------------");
+
+        // 2. Bind a value to it ("Alice") and run a block of code
+        ScopedValue.where(USER, "Alice").run(() -> {
+
+            // 3. Inside this block, USER is bound to "Alice"
+            System.out.println("Inside scope: " + USER.get());
+
+        });
+
+
+        // Note: If you tried to call USER.get() out here,
+        // it would throw the exception again because the scope has ended!
+
+        System.out.println("----------------------------------------------------");
+    }
+}
